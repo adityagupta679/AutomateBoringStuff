5e6eafe4d285e38c7e619c669d16f47c968d0e99
adityagupta679 <adityagupta679@gmail.com>
2016-11-27 15:02:45 +0530

    Adding Mini Project : MapIt

---
+++ Mini Projects/mapIt.py
@@ -0,0 +1,23 @@
+#! python2
+
+# A Program to automatically open Google Maps with address given in cmd line arguments 
+# or the clipboard contents
+
+import webbrowser,sys
+
+# if cmd line arguments are given
+if len(sys.argv) >1:
+	search_address= ' '.join(sys.argv[1:])
+
+# TODO: Get address from clipboard
+
+
+# address to open
+base_url="https://www.google.com/maps/place/"
+new_url=base_url+search_address
+print new_url
+
+# opening the web browser
+
+
+webbrowser.open(new_url)
