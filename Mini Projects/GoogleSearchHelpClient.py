145c481beca30605534716383c910deea1649f21
adityagupta679 <adityagupta679@gmail.com>
2016-12-06 17:36:43 +0530

    Adding Google Seach Help Client Updated readme as well.

---
+++ Mini Projects/GoogleSearchHelpClient.py
@@ -0,0 +1,52 @@
+#! pyhton2
+
+# A Web scraping program to query google for a search term and 
+# open the First few links automatically in the browser . 
+# Hence avoiding the trouble to open the links by hand .
+
+
+import sys,requests, bs4 , pyperclip, webbrowser
+
+search_term="" 
+# Checking cmd line arguments , if none given, 
+# consider the text in the clipboard as a search term
+
+if len(sys.argv)>1 :
+	search_term=" ".join(sys.argv[1:])
+	
+else:
+	search_term=pyperclip.paste()
+# print search_term
+
+base_url="https://www.google.co.in/search?q="
+search_url=base_url+search_term
+
+# Quering Google for the term 
+
+search_results= requests.get(search_url)
+
+#  Check for errors while executing the search request.
+search_results.raise_for_status()
+
+soup=bs4.BeautifulSoup(search_results.text,"html.parser")
+links= soup.select('h3.r a' )
+
+# extract the urls from the links
+urls=[]
+for link in links :
+	href=link.get('href')
+	urls.append(href[href.find("q=")+2:href.find("&sa")])
+print "Openinng Google results for the Search term: {}. \nThe links are: {} ".format(search_term,str(urls))
+# open the links in the webbrowser
+for url in urls[:6]:
+	webbrowser.open(url)
+
+# TODO:
+# Handle issue of youtube links not opening correctly ,
+# mainly an issue of character encoding or conversion 
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -8,4 +8,5 @@ Implementation of Programming projects examples from [Automate the Boring Stuff 
 1. __mapIt:__    *A Program to automatically open Google Maps with the address given in cmd line arguments or the clipboard contents.*  
 2. __WebPageDownload:__  * A program to download webpages and saving them to local harddisk using requests library. *
   TODO: [ ] Check Encoding Error while printing the webpage received.  
- 
+3. __GoogleSearchHelpCLient:__ *# A Web scraping program to query google for a search term and open the First few links automatically in the browser . Hence avoiding the trouble to open the links by hand.* 
+  TODO: [ ] Handle issue of youtube links not opening correctly ,mainly an issue of character encoding or conversion.
